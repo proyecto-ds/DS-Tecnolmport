183c093f69134cf3cae62df8c967590b10373d53
juanj97 <juanj121297e@gmail.com>
2019-08-23 22:07:12 -0500

    documento final y arreglo de base

--- Tecnolmport/src/Singleton/DBConnection.java
+++ Tecnolmport/src/Singleton/DBConnection.java
@@ -23,7 +23,7 @@ public class DBConnection {
     private final String url = "jdbc:mysql://localhost:3306/TecnolmportDS?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private final String user = "root";
 
-    private final String pass = "1997ra"; 
+    private final String pass = "betoelperro"; 
 
 
 
